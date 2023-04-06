412441004935c20ee99d64906405339e3a7aebd9
Charles Junior <charlesjunnr@gmail.com>
2023-04-06 01:12:38 -0300

    Add project files.

---
+++ ExercicioTrianguloOrientadoObjeto/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ExercicioTrianguloOrientadoObjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo valores = new Triangulo();
+
+            Console.WriteLine(" ------- Triângulos -------");
+            Console.WriteLine(" -- Digite um valor para X: ");
+            valores.valorXis = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine(" -- Digite um valor para Y: ");
+            valores.valorYpsolon = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" -- Digite um valor para Z: ");
+            valores.valorZeh = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Verificando os valores...{valores.VerificarTriangulo()}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExercicioTrianguloOrientadoObjeto/Triangulo.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioTrianguloOrientadoObjeto
+{
+    internal class Triangulo
+    {
+        public int valorXis, valorYpsolon, valorZeh;
+        public string tipoTriangulo;
+
+        public string DesenharTriangulo()
+        {
+            
+            if (tipoTriangulo == "Escaleno")
+            {
+                return ("\n   ESCALENO    " +
+                        "\n   |\\          " +
+                        "\n   | \\         " +
+                        "\n   |  \\        " +
+                        "\n   |   \\       " +
+                        "\n   |    \\      " +
+                        "\n   |     \\     " +
+                        "\n   |      \\    " +
+                        "\n   |_______\\   ");
+
+            }
+            else if (tipoTriangulo == "Isósceles")
+            {
+                return ("\n       ISÓSCELES       " +
+                        "\n         /|\\         " +
+                        "\n        / | \\        " +
+                        "\n       /  |  \\       " +
+                        "\n      /   |   \\      " +
+                        "\n     /    |    \\     " +
+                        "\n    /     |     \\    " +
+                        "\n   /______|______\\   ");
+            }
+            else if (tipoTriangulo == "Equilátero")
+            {
+                return ("\n      EQUILÁTERO        " +
+                        "\n           .          " +
+                        "\n          / \\          " +
+                        "\n         /   \\         " +
+                        "\n        /     \\        " +
+                        "\n       /       \\       " +
+                        "\n      /         \\      " +
+                        "\n     /           \\     " +
+                        "\n    /             \\    " +
+                        "\n   /               \\   " +
+                        "\n  /_________________\\  ");
+            }
+            else
+            {
+                return ("Não há um desenho para esse triângulo!");
+            }
+        }
+        public string VerificarTriangulo()
+        {   
+            
+            if (valorXis == valorYpsolon && valorXis == valorZeh)
+            {
+                tipoTriangulo = "Equilátero";
+                return ($"É um triângulo equilátero! Todos os lados são iguais!{DesenharTriangulo()}");
+            }
+            else if (
+                   valorXis == valorYpsolon && valorXis != valorZeh
+                || valorXis == valorZeh && valorXis != valorYpsolon
+                || valorZeh == valorYpsolon && valorZeh != valorXis)
+            {
+                tipoTriangulo = "Isósceles";
+                return ($"É um triângulo isósceles! Dois lados iguais e uma base diferente!{DesenharTriangulo()}");
+            }
+
+            if (valorXis != valorYpsolon && valorYpsolon != valorZeh && valorZeh != valorXis)
+            {
+                tipoTriangulo = "Escaleno";
+                return ($"É um triângulo escaleto! Todos os lados são diferentes!{DesenharTriangulo()}");
+            }
+            else
+            {
+                return ("Valores inválidos!");
+            }
+
+
+        }
+    }
+}
